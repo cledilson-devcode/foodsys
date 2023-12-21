7f219378048575f8883ae05705bfeb574e81a81c
cledilson <cledilsondevoffice@gmail.com>
2023-12-21 17:37:06 -0300

    feat: Implementando CozinhaController com tratamento de respoastas dos protocolocas HTTP com base em alguns status HTTP.

--- src/main/java/com/cledilsondevcode/foodsys/api/controller/CozinhaController.java
+++ src/main/java/com/cledilsondevcode/foodsys/api/controller/CozinhaController.java
@@ -4,8 +4,13 @@ package com.cledilsondevcode.foodsys.api.controller;
 import com.cledilsondevcode.foodsys.api.model.CozinhasXmlWrapper;
 import com.cledilsondevcode.foodsys.domain.model.Cozinha;
 import com.cledilsondevcode.foodsys.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,12 +30,63 @@ public class CozinhaController {
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
     public CozinhasXmlWrapper listarXml(){
+
         return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId){
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId){
+
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinha != null){
+            return ResponseEntity.ok(cozinha);
+        }
+
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.notFound().build();
+
+
+//        return  ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//        return ResponseEntity.ok(cozinha);
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+//
+//        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+        return cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha){
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+        if (cozinhaAtual != null) {
+            cozinhaAtual.setNome(cozinha.getNome());
+//        BeanUtils.copyProperties(cozinha, cozinhaAtual, "id"); // caso tenha muitas propriedades, utilizar essa opção para atualizar após a busca do id no banco
+            cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId){
+        try {
+            Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+            if (cozinha != null) {
+                cozinhaRepository.remover(cozinha);
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.notFound().build();
+        }catch (DataIntegrityViolationException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
 }
